2db30cb40a6773c92cb3ba2e776038e1a13abebc
Quentin Stiévenart <quentin.stievenart@gmail.com>
2019-11-21 17:53:41 +0100

    Fix store and load, check stack size in transfer function

--- wasmtaint.ml
+++ wasmtaint.ml
@@ -725,52 +725,75 @@ module Transfer = struct
       state
     | Drop ->
       let (_, vstack') = Domain.pop state.vstack in
+      assert (List.length vstack' = List.length state.vstack - 1);
       { state with vstack = vstack' }
     | LocalGet x ->
-      { state with vstack = (Domain.get_local state.locals x) :: state.vstack }
+      let vstack' = (Domain.get_local state.locals x) :: state.vstack in
+      assert (List.length vstack' = 1 + List.length state.vstack);
+      { state with vstack = vstack' }
     | LocalSet x ->
       let (v, vstack') = Domain.pop state.vstack in
+      assert (List.length vstack' = List.length state.vstack - 1);
       { state with vstack = vstack';
                    locals = Domain.set_local state.locals x v }
     | LocalTee x ->
       let (v, vstack') = Domain.pop state.vstack in
-      instr_transfer (LocalSet x) { state with vstack = v :: v :: vstack' }
+      let res = instr_transfer (LocalSet x) { state with vstack = v :: v :: vstack' } in
+      assert (List.length res.vstack = List.length state.vstack);
+      res
     | GlobalGet x ->
-      { state with vstack = (Domain.get_global state.globals x) :: state.vstack }
+      let vstack' = (Domain.get_global state.globals x) :: state.vstack in
+      assert (List.length vstack' = List.length state.vstack + 1);
+      { state with vstack = vstack' }
     | GlobalSet x ->
       let (v, vstack') = Domain.pop state.vstack in
+      assert (List.length vstack' = List.length state.vstack - 1);
       { state with vstack = vstack';
                    globals = Domain.set_global state.globals x v }
     | Br _ ->
       state
     | BrIf _ ->
       let (_, vstack') = Domain.pop state.vstack in
+      assert (List.length vstack' = List.length state.vstack - 1);
       { state with vstack = vstack' }
     | Return -> state
     | Const v ->
-      { state with vstack = v :: state.vstack }
+      let vstack' = v :: state.vstack in
+      assert (List.length vstack' = List.length state.vstack + 1);
+      { state with vstack = vstack' }
     | Compare rel ->
       let (v1, vstack') = Domain.pop state.vstack in
       let (v2, vstack'') = Domain.pop vstack' in
       let v = Relop.eval rel v1 v2 in
-      { state with vstack = v :: vstack'' }
+      let vstack''' = v :: vstack'' in
+      assert (List.length vstack''' = List.length state.vstack - 1);
+      { state with vstack =  vstack''' }
     | Binary bin ->
       let (v1, vstack') = Domain.pop state.vstack in
       let (v2, vstack'') = Domain.pop vstack' in
       let v = Binop.eval bin v1 v2 in
-      { state with vstack = v :: vstack'' }
+      let vstack''' = v :: vstack'' in
+      assert (List.length vstack''' = List.length state.vstack - 1);
+      { state with vstack = vstack''' }
     | Test test ->
       let (v, vstack') = Domain.pop state.vstack in
       let v' = Testop.eval test v in
-      { state with vstack = v' :: vstack' }
+      let vstack'' = v' :: vstack' in
+      assert (List.length vstack'' = List.length state.vstack);
+      { state with vstack = vstack'' }
     | Load op ->
       (* TODO: for now, we just return the top value of the expected type *)
+      let (_, vstack') = Domain.pop state.vstack in
       let c = Value.top op.typ in (* value of the correct type *)
-      { state with vstack = c :: state.vstack }
+      let vstack'' = c :: vstack' in
+      assert (List.length vstack'' = List.length state.vstack);
+      { state with vstack = vstack'' }
     | Store _op ->
       (* TODO: for now, we just ignore the store *)
       let (_, vstack') = Domain.pop state.vstack in
-      { state with vstack = vstack' }
+      let (_, vstack'') = Domain.pop vstack' in
+      assert (List.length vstack'' = List.length state.vstack - 2);
+      { state with vstack = vstack'' }
     | Block _ -> failwith "shouldn't happen"
     | Loop _ -> failwith "shouldn't happen"
     | Call _ -> failwith "shouldn't happen"
