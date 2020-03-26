2797f2fa0f3d92834bf8e03068b9ea0b1ccea990
Quentin Stiévenart <quentin.stievenart@gmail.com>
2020-03-26 18:23:43 +0100

    WIP update memory

--- lib/memory.ml
+++ lib/memory.ml
@@ -1,41 +1,15 @@
 open Core_kernel
 
-(* Representation of the memory using separation logic *)
-type byte =
-  | ByteInValue of (Value.t * int) (* (val, byte_position) *)
+type t = unit
 [@@deriving sexp, compare]
-let byte_to_string (b : byte) : string = match b with
-  | ByteInValue (v, b) -> begin match v.value with
-      | Value.Bottom -> "Bottom"
-      | Value.Const n -> Printf.sprintf "%d[%d]" (Option.value_exn (Int32.to_int n)) b
-      | Value.Int -> Printf.sprintf "%s[%d]" (Value.sources_to_string v.sources) b
-    end
 
-type formula = (byte * byte) list
-[@@deriving sexp, compare]
-let formula_to_string (f : formula) : string = match f with
-  | [] -> "emp"
-  | _ -> String.concat ~sep:" * " (List.map ~f:(fun (b1, b2) -> Printf.sprintf "%s -> %s" (byte_to_string b1) (byte_to_string b2)) f)
+let initial = ()
 
-let maps_to (f : formula) (b : byte) = Option.map ~f:(fun (_, b2) -> b2) (List.find f ~f:(fun (b1, _) -> compare_byte b b1 = 0))
+let load (_m : t) (_addr : Value.t)  = failwith "TODO"
 
-let formula_mapsto_4bytes (f : formula) (i : Value.t) (offset : int) : Value.t option =
-  match (maps_to f (ByteInValue (i, offset)),
-         maps_to f (ByteInValue (i, offset + 1)),
-         maps_to f (ByteInValue (i, offset + 2)),
-         maps_to f (ByteInValue (i, offset + 3))) with
-  | Some (ByteInValue (c0, _)), Some (ByteInValue (c1, _)), Some (ByteInValue (c2, _)), Some (ByteInValue (c3, _))
-    when Value.compare c0 c1 = 0 && Value.compare c1 c2 = 0 && Value.compare c2 c3 = 0 -> Some c0
-  | None, None, None, None -> None
-  | _ -> failwith "TODO: formula_mapsto_4bytes"
-let add_mapsto (f : formula) (b1 : byte) (b2 : byte) = (b1, b2) :: (List.filter ~f:(fun (b1', _) -> compare_byte b1 b1' <> 0) f)
+let store (_m : t) (_addr : Value.t) (_value : Value.t) = failwith "TODO"
 
+let to_string (_m : t) : string = "M"
 
-type t = formula
-[@@deriving sexp, compare]
-let to_string (m : t) : string = formula_to_string m
+let join (m1 : t) (_m2 : t) : t = m1
 
-
-let join (m1 : t) (m2 : t) : t =
-  (* TODO: if m2 redefines elements from m1, fail? or do what? *)
-  List.fold_left ~init:m1 ~f:(fun f (b1, b2) -> add_mapsto f b1 b2) m2
--- lib/transfer.ml
+++ lib/transfer.ml
@@ -3,91 +3,78 @@ open Helpers
 
 (* Transfer function for an instruction.
    Takes as argument the instruction `i`, and the state before the instruction (prestate).
-   Returns a formula and the resulting state (poststate). The formula indicates what has to be assumed in the prestate *)
-let rec instr_transfer (i : Instr.t) (state : Domain.state) : (Memory.formula * Domain.state) =
+   Returns the resulting state (poststate).
+*)
+let rec instr_transfer (i : Instr.t) (state : Domain.state) : Domain.state =
   match i with
   | Nop ->
-    [], state
+    state
   | Drop ->
     let (_, vstack') = Vstack.pop state.vstack in
     assert (List.length vstack' = List.length state.vstack - 1);
-    [], { state with vstack = vstack' }
+    { state with vstack = vstack' }
   | LocalGet x ->
     let vstack' = (Locals.get_local state.locals x) :: state.vstack in
     assert (List.length vstack' = 1 + List.length state.vstack);
-    [], { state with vstack = vstack' }
+    { state with vstack = vstack' }
   | LocalSet x ->
     let (v, vstack') = Vstack.pop state.vstack in
     assert (List.length vstack' = List.length state.vstack - 1);
-    [], { state with vstack = vstack';
+    { state with vstack = vstack';
                  locals = Locals.set_local state.locals x v }
   | LocalTee x ->
     let (v, vstack') = Vstack.pop state.vstack in
-    let (f, state') = instr_transfer (LocalSet x) { state with vstack = v :: v :: vstack' } in
+    let state' = instr_transfer (LocalSet x) { state with vstack = v :: v :: vstack' } in
     assert (List.length state'.vstack = List.length state.vstack);
-    f, state'
+    state'
   | GlobalGet x ->
     let vstack' = (Globals.get_global state.globals x) :: state.vstack in
     assert (List.length vstack' = List.length state.vstack + 1);
-    [], { state with vstack = vstack' }
+    { state with vstack = vstack' }
   | GlobalSet x ->
     let (v, vstack') = Vstack.pop state.vstack in
     assert (List.length vstack' = List.length state.vstack - 1);
-    [], { state with vstack = vstack';
-                     globals = Globals.set_global state.globals x v }
+    { state with vstack = vstack';
+                 globals = Globals.set_global state.globals x v }
   | Br _ ->
-    [], state
+    state
   | BrIf _ ->
     let (_, vstack') = Vstack.pop state.vstack in
     assert (List.length vstack' = List.length state.vstack - 1);
-    [], { state with vstack = vstack' }
-  | Return -> [], state
+    { state with vstack = vstack' }
+  | Return ->
+    state
   | Const v ->
     let vstack' = v :: state.vstack in
     assert (List.length vstack' = List.length state.vstack + 1);
-    [], { state with vstack = vstack' }
+    { state with vstack = vstack' }
   | Compare rel ->
     let (v1, vstack') = Vstack.pop state.vstack in
     let (v2, vstack'') = Vstack.pop vstack' in
     let v = Relop.eval rel v1 v2 in
     let vstack''' = v :: vstack'' in
     assert (List.length vstack''' = List.length state.vstack - 1);
-    [], { state with vstack =  vstack''' }
+    { state with vstack =  vstack''' }
   | Binary bin ->
     let (v1, vstack') = Vstack.pop state.vstack in
     let (v2, vstack'') = Vstack.pop vstack' in
     let v = Binop.eval bin v1 v2 in
     let vstack''' = v :: vstack'' in
     assert (List.length vstack''' = List.length state.vstack - 1);
-    [], { state with vstack = vstack''' }
+    { state with vstack = vstack''' }
   | Test test ->
     let (v, vstack') = Vstack.pop state.vstack in
     let v' = Testop.eval test v in
     let vstack'' = v' :: vstack' in
     assert (List.length vstack'' = List.length state.vstack);
-    [], { state with vstack = vstack'' }
+    { state with vstack = vstack'' }
   | Load op ->
-    assert (op.sz = None); (* We only support N = 32 for now *)
+    assert (op.sz = None); (* We only support N = 32 for now. TODO: move assumption to memory.ml *)
     let (i, vstack') = Vstack.pop state.vstack in
-    let (f, state') = begin match Memory.formula_mapsto_4bytes state.memory i op.offset with
-      | Some c ->
-        (* If there is already a value in the heap formula, use it *)
-        let vstack'' = c :: vstack' in
-        [], { state with vstack = vstack'' }
-      | None ->
-        (* Otherwise, expand the heap formula *)
-        (* TODO: this expansion should be on the pre formula, not the post! *)
-        let c = Value.top op.typ (Value.Source.Heap i.value) in (* value of the correct type *)
-        let vstack'' = c :: vstack' in
-        let f = Memory.[(ByteInValue (i, op.offset), ByteInValue (c, 0));
-                        (ByteInValue (i, op.offset + 1), ByteInValue (c, 1));
-                        (ByteInValue (i, op.offset + 2), ByteInValue (c, 2));
-                        (ByteInValue (i, op.offset + 3), ByteInValue (c, 3))] in
-        let memory' = Memory.join state.memory f in
-        f, { state with vstack = vstack''; memory = memory'}
-    end in
+    let c = Memory.load state.memory i (* op  *) in
+    let state' = { state with vstack = c :: vstack' } in
     assert (List.length state'.vstack = List.length state.vstack);
-    f, state'
+    state'
   | Store op ->
     (* Pop the value t.const c from the stack *)
     let (c, vstack') = Vstack.pop state.vstack in
@@ -95,36 +82,27 @@ let rec instr_transfer (i : Instr.t) (state : Domain.state) : (Memory.formula * 
     let (i, vstack'') = Vstack.pop vstack' in
     (* let b be the byte sequence of c *)
     assert (op.sz = None); (* We only support N = 32 for now *)
-    (* "Replace the bytes mem.data[ea:N/8] with b*" -> we remember that in the heap formula *)
-    (* The formula is:
-       heap * ea -> c0 * ea+1 -> c1 * ea+2 -> c2 * ea+3 -> c3
-       where c is formed of the bytes c0, c1, c2, c3 *)
-    (* TODO: expand pre-state with formula *)
-    let memory' = Memory.join state.memory
-        [(ByteInValue (i, op.offset), ByteInValue (c, 0));
-         (ByteInValue (i, op.offset + 1), ByteInValue (c, 1));
-         (ByteInValue (i, op.offset + 2), ByteInValue (c, 2));
-         (ByteInValue (i, op.offset + 3), ByteInValue (c, 3))] in
+    let memory' = Memory.store state.memory c i (* op *) in
     assert (List.length vstack'' = List.length state.vstack - 2);
-    [], { state with vstack = vstack''; memory = memory' }
+    { state with vstack = vstack''; memory = memory' }
   | Block _ -> failwith "shouldn't happen"
   | Loop _ -> failwith "shouldn't happen"
   | Call _ -> failwith "shouldn't happen"
 
-let transfer (b : Basic_block.t) (state : Domain.state) (summaries: Summary.t IntMap.t) : (Memory.formula * Domain.state) =
+let transfer (b : Basic_block.t) (state : Domain.state) (summaries: Summary.t IntMap.t) : Domain.state =
   match b.sort with
   | Normal ->
-    List.fold_left b.instrs ~init:([], state) ~f:(fun (f, prestate) i ->
-        let (f', poststate) = instr_transfer i prestate in
+    List.fold_left b.instrs ~init:state ~f:(fun prestate i ->
+        let poststate = instr_transfer i prestate in
         Printf.printf "pre: %s\ninstr: %s\npost: %s\n" (Domain.to_string prestate) (Instr.to_string i) (Domain.to_string poststate);
-        (f @ f', poststate)
+        poststate
       )
   | Function -> begin match b.instrs with
       | Call f :: [] ->
         (* We encounter a function call, retrieve its summary and apply it *)
         (* We assume all summaries are defined *)
         let summary = IntMap.find_exn summaries f in
-        [], Summary.apply summary f state
+        Summary.apply summary f state
       | _ -> failwith "Invalid function block"
     end
-  | _ -> [], state
+  | _ -> state
