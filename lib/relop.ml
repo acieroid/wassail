cb43b84c80fb80487f8a492691e0d40cfc40efdf
Quentin Stiévenart <quentin.stievenart@gmail.com>
2020-05-22 10:58:18 +0200

    Refactor relop

--- lib/relop.ml
+++ lib/relop.ml
@@ -2,36 +2,46 @@ open Core_kernel
 open Wasm
 open Value
 
+exception UnsupportedRelOp of string
+
 (** Relational operation *)
 module T = struct
-  type t =
-    | I32Eq
-    | I32Ne
-    | I32LtS
-    | I32GtS
-    | I32LeS
-    | I32GeS (* XXX: others *)
+  type op = Eq | Ne | LtS | LtU | GtS | GtU | LeS | LeU | GeS | GeU
+  [@@deriving sexp, compare]
+  type t = { op: op; typ: Type.t }
   [@@deriving sexp, compare]
 end
 include T
+
 let of_wasm (r : Ast.relop) : t =
   match r with
-  | I32 Eq -> I32Eq
-  | I32 Ne -> I32Ne
-  | I32 LtS -> I32LtS
-  | I32 GtS -> I32GtS
-  | I32 LeS -> I32LeS
-  | I32 GeS -> I32GeS
-  | I32 _ -> failwith "unsupported relational operation"
+  | I32 op -> { typ = I32; op = match op with
+      | Eq -> Eq
+      | Ne -> Ne
+      | LtS -> LtS
+      | LtU -> LtU
+      | GtS -> GtS
+      | GtU -> GtU
+      | LeS -> LeS
+      | LeU -> LeU
+      | GeS -> GeS
+      | GeU -> GeU
+    }
   | _ -> failwith "unsupported type"
 let to_string (r : t) : string =
-  match r with
-  | I32Eq -> "i32.eq"
-  | I32Ne -> "i32.ne"
-  | I32LtS -> "i32.lt_s"
-  | I32GtS -> "i32.gt_s"
-  | I32LeS -> "i32.le_s"
-  | I32GeS -> "i32.ge_s"
+  Printf.sprintf "%s.%s"
+    (Type.to_string r.typ)
+    (match r.op with
+     | Eq -> "eq"
+     | Ne -> "ne"
+     | LtS -> "lt_s"
+     | LtU -> "lt_u"
+     | GtS -> "gt_s"
+     | GtU -> "gt_u"
+     | LeS -> "le_s"
+     | LeU -> "le_u"
+     | GeS -> "ge_s"
+     | GeU -> "ge_u")
 
 let eq (v1 : Value.t) (v2 : Value.t) : Value.t = match (v1.value, v2.value) with
   | (Symbolic (Const n1), Symbolic (Const n2)) when PrimValue.(n1 = n2) -> const (PrimValue.of_int_t n1 1)
@@ -62,9 +72,10 @@ let ge_s (v1 : Value.t) (v2 : Value.t) : Value.t = match (v1.value, v2.value) wi
 
 let eval (r : t) (v1 : Value.t) (v2 : Value.t) : Value.t =
   match r with
-  | I32Eq -> eq v1 v2
-  | I32Ne -> ne v1 v2
-  | I32LtS -> lt_s v1 v2
-  | I32GtS -> gt_s v1 v2
-  | I32LeS -> le_s v1 v2
-  | I32GeS -> ge_s v1 v2
+  | { typ = I32; op = Eq } -> eq v1 v2
+  | { typ = I32; op = Ne } -> ne v1 v2
+  | { typ = I32; op = LtS } -> lt_s v1 v2
+  | { typ = I32; op = GtS } -> gt_s v1 v2
+  | { typ = I32; op = LeS } -> le_s v1 v2
+  | { typ = I32; op = GeS } -> ge_s v1 v2
+  | _ -> failwith (Printf.sprintf "unsupported relop: %s" (to_string r))
