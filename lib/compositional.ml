23056b53f44f7fb6ade9ba3818904f0aa7cf5bf9
Quentin Stiévenart <quentin.stievenart@gmail.com>
2020-06-08 16:35:30 +0200

    Remove old file

--- lib/compositional.ml
+++
@@ -1,8 +0,0 @@
-
-(* Compositionally analyzes a set of CFGs *)
-(* TODO: inspired by inter-fixpoint, but only need one pass over each functionn? (except maybe for mutually recursive ones? *)
-(* This is actually all done in interfixpoint currently *)
-      
-
-  
-  
