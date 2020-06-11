f9bd725c0489b30d72eaf9d93cd12e5a935657ec
Quentin Stiévenart <quentin.stievenart@gmail.com>
2020-06-11 15:26:44 +0200

    Don't analyze imported functions

--- lib/inter_fixpoint.ml
+++ lib/inter_fixpoint.ml
@@ -23,30 +23,36 @@ let analyze (cfgs : Cfg.t IntMap.t) (module_ : Wasm_module.t) : unit =
     else
       (* Next CFG to analyze *)
       let cfg_idx = IntSet.min_elt_exn worklist in
-      let cfg = IntMap.find_exn cfgs cfg_idx in
-      (* The arguments to this CFG. They all map to top because we perform a compositional analysis *)
-      Printf.printf "Analyzing cfg %d (name: %s)\n" cfg_idx cfg.name;
-      (* Perform intra-procedural analysis *)
-      let results = Intra_fixpoint.analyze cfg !summaries module_ in
-      let out_state = Intra_fixpoint.out_state cfg results in
-      (* Check difference with previous state, if there was any *)
-      let previous_results = IntMap.find_exn !data cfg_idx in
-      match previous_results with
-      | Some res when Domain.compare_state out_state (Intra_fixpoint.out_state cfg res) = 0 ->
-        (* Same results as before, we can just recurse without having to recompute globals nor memory *)
+      if cfg_idx < module_.nimports then begin
+        Printf.printf "Not analyzing cfg %d (it is an imported function)" cfg_idx;
         fixpoint (IntSet.remove worklist cfg_idx)
-      | _ ->
-        (* Result differed, we have to add all callees and callers to the worklist.
-           Callers because the analyzed function could have modified globals/memory that will be read by the caller.
-           Callees for the same reason. *)
-        let callees = Cfg.callees cfg in
-        let callers = Cfg.callers cfgs cfg in
-        let summary = Summary.make cfg out_state in
-        Printf.printf "Final summary is: %s\n" (Summary.to_string summary);
-        summaries := IntMap.set !summaries ~key:cfg.idx ~data:summary;
-        (* Update data of the analysis and recurse *)
-        data := IntMap.set !data ~key:cfg_idx ~data:(Some results);
-        fixpoint (IntSet.union (IntSet.remove worklist cfg_idx) (IntSet.union callees callers))
+      end else begin
+        let cfg = IntMap.find_exn cfgs cfg_idx in
+        (* The arguments to this CFG. They all map to top because we perform a compositional analysis *)
+        Printf.printf "Analyzing cfg %d (name: %s)\n" cfg_idx cfg.name;
+        Stdlib.flush_all ();
+        (* Perform intra-procedural analysis *)
+        let results = Intra_fixpoint.analyze cfg !summaries module_ in
+        let out_state = Intra_fixpoint.out_state cfg results in
+        (* Check difference with previous state, if there was any *)
+        let previous_results = IntMap.find_exn !data cfg_idx in
+        match previous_results with
+        | Some res when Domain.compare_state out_state (Intra_fixpoint.out_state cfg res) = 0 ->
+          (* Same results as before, we can just recurse without having to recompute globals nor memory *)
+          fixpoint (IntSet.remove worklist cfg_idx)
+        | _ ->
+          (* Result differed, we have to add all callees and callers to the worklist.
+             Callers because the analyzed function could have modified globals/memory that will be read by the caller.
+             Callees for the same reason. *)
+          let callees = Cfg.callees cfg in
+          let callers = Cfg.callers cfgs cfg in
+          let summary = Summary.make cfg out_state in
+          Printf.printf "Final summary is: %s\n" (Summary.to_string summary);
+          summaries := IntMap.set !summaries ~key:cfg.idx ~data:summary;
+          (* Update data of the analysis and recurse *)
+          data := IntMap.set !data ~key:cfg_idx ~data:(Some results);
+          fixpoint (IntSet.union (IntSet.remove worklist cfg_idx) (IntSet.union callees callers))
+      end
   in
   (* Initial summaries are all empty *)
   summaries := Summary.initial_summaries cfgs module_;
