3e672ce7fe6ac99d90558e556bdfc1bb7c670e7b
Quentin Stiévenart <quentin.stievenart@gmail.com>
2021-04-16 09:36:37 +0200

    [codegen] Keep a visited set to avoid infinite generation loops

--- lib/cfg/codegen.ml
+++ lib/cfg/codegen.ml
@@ -1,4 +1,5 @@
 open Core_kernel
+open Helpers
 
 let true_edge (edges : Cfg.Edge.t list) : Cfg.Edge.t option =
   List.find edges
@@ -33,68 +34,69 @@ let relevant_successor (cfg : unit Cfg.t) (block : unit Basic_block.t) : int opt
    There can be multiple successor for blocks such as br_if etc., but only one
    is returned: the one that is important for generating the code in the right
    order (e.g., the one that does not perform the jump).  *)
-let rec codegen_block (cfg : unit Cfg.t) (block : unit Basic_block.t) : unit Instr.t list * int option =
-  let control i = Instr.Control ({ instr = i; annotation_before = (); annotation_after = (); label = { section = Dummy; id = 0 }}) in
-  let data i = Instr.Data ({ instr = i; annotation_before = (); annotation_after = (); label = { section = Dummy; id = 0 }}) in
-  match block.block_kind with
-  | Some (LoopEntry (bt, arity)) ->
-    let successors = Cfg.successors cfg block.idx in
-    assert (List.length successors = 1);
-    let succ = List.hd_exn successors in
-    let exit_block = Cfg.corresponding_exit_exn cfg block.idx in
-    Printf.printf "generating block %d, now generating until %d\n" block.idx exit_block;
-    (* TODO: we may not be able to reach exit_block in case of unconditional loops.
-       What to do then? *)
-    let (body, _) = codegen_until cfg succ exit_block in
-    [control (Instr.Loop (bt, arity, body))], Some exit_block
-  | Some BlockEntry (bt, arity) ->
-    let exit_block = Cfg.corresponding_exit_exn cfg block.idx in
-    let successors = Cfg.successors cfg block.idx in
-    assert (List.length successors = 1);
-    let succ = List.hd_exn successors in
-    let (body, _) = codegen_until cfg succ exit_block in
-    [control (Block (bt, arity, body))], Some exit_block
-  | Some LoopExit
-  | Some BlockExit
-  | Some IfExit ->
-    [], relevant_successor cfg block
-  | None ->
-    match block.content with
-    | Data instrs -> List.map instrs ~f:(fun i -> data i.instr), relevant_successor cfg block
-    | Control { instr = Merge; _ } -> [], relevant_successor cfg block
-    | Control { instr = Block _; _ }
-    | Control { instr = Loop _; _ } -> failwith "Not expecting block or loop instructions in CFG block"
-    | Control { instr = If (bt, arity, _, _); _ } ->
-      let edges = Cfg.outgoing_edges cfg block.idx in
-      assert (List.length edges = 2);
-      begin match (true_edge edges, false_edge edges) with
-        | (Some (t, _), Some (f, _)) ->
-          let exit_block = Cfg.corresponding_exit_exn cfg block.idx in
-          let (t_code, exit_block_idx) = codegen_until cfg t exit_block in
-          let (f_code, exit_block_idx') = codegen_until cfg f exit_block in
-          assert (Option.equal (=) exit_block_idx exit_block_idx');
-          [control (If (bt, arity, t_code, f_code))], exit_block_idx
-        | _ -> failwith "Should not happen"
-      end
-    | Control { instr; _ } ->
-      [control instr], relevant_successor cfg block
-and codegen_until (cfg : unit Cfg.t) (start_block_idx : int) (stop_block_idx : int) : unit Instr.t list * int option =
+let rec codegen_block (cfg : unit Cfg.t) (block : unit Basic_block.t) (visited : IntSet.t) : unit Instr.t list * int option =
+  if IntSet.mem visited block.idx then
+    [], None
+  else
+    let control i = Instr.Control ({ instr = i; annotation_before = (); annotation_after = (); label = { section = Dummy; id = 0 }}) in
+    let data i = Instr.Data ({ instr = i; annotation_before = (); annotation_after = (); label = { section = Dummy; id = 0 }}) in
+    match block.block_kind with
+    | Some (LoopEntry (bt, arity)) ->
+      let successors = Cfg.successors cfg block.idx in
+      assert (List.length successors = 1);
+      let succ = List.hd_exn successors in
+      let exit_block = Cfg.corresponding_exit_exn cfg block.idx in
+      Printf.printf "generating block %d, now generating until %d\n" block.idx exit_block;
+      let (body, _) = codegen_until cfg succ exit_block (IntSet.add visited block.idx) in
+      [control (Instr.Loop (bt, arity, body))], Some exit_block
+    | Some BlockEntry (bt, arity) ->
+      let exit_block = Cfg.corresponding_exit_exn cfg block.idx in
+      let successors = Cfg.successors cfg block.idx in
+      assert (List.length successors = 1);
+      let succ = List.hd_exn successors in
+      let (body, _) = codegen_until cfg succ exit_block (IntSet.add visited block.idx) in
+      [control (Block (bt, arity, body))], Some exit_block
+    | Some LoopExit
+    | Some BlockExit
+    | Some IfExit ->
+      [], relevant_successor cfg block
+    | None ->
+      match block.content with
+      | Data instrs -> List.map instrs ~f:(fun i -> data i.instr), relevant_successor cfg block
+      | Control { instr = Merge; _ } -> [], relevant_successor cfg block
+      | Control { instr = Block _; _ }
+      | Control { instr = Loop _; _ } -> failwith "Not expecting block or loop instructions in CFG block"
+      | Control { instr = If (bt, arity, _, _); _ } ->
+        let edges = Cfg.outgoing_edges cfg block.idx in
+        assert (List.length edges = 2);
+        begin match (true_edge edges, false_edge edges) with
+          | (Some (t, _), Some (f, _)) ->
+            let exit_block = Cfg.corresponding_exit_exn cfg block.idx in
+            let (t_code, exit_block_idx) = codegen_until cfg t exit_block (IntSet.add visited block.idx) in
+            let (f_code, exit_block_idx') = codegen_until cfg f exit_block (IntSet.add visited block.idx) in
+            assert (Option.equal (=) exit_block_idx exit_block_idx');
+            [control (If (bt, arity, t_code, f_code))], exit_block_idx
+          | _ -> failwith "Should not happen"
+        end
+      | Control { instr; _ } ->
+        [control instr], relevant_successor cfg block
+and codegen_until (cfg : unit Cfg.t) (start_block_idx : int) (stop_block_idx : int) (visited : IntSet.t) : unit Instr.t list * int option =
   let return l = List.concat (List.rev l) in
-  let rec loop (instrs : unit Instr.t list list) (block_idx : int) : unit Instr.t list * int option =
+  let rec loop (instrs : unit Instr.t list list) (block_idx : int) (visited : IntSet.t) : unit Instr.t list * int option =
     Printf.printf "loop on block %d\n" block_idx;
     let block = Cfg.find_block_exn cfg block_idx in
     if block_idx = stop_block_idx then begin
       (* We stop generating here *)
       return instrs, relevant_successor cfg block
     end else
-      match codegen_block cfg block with
-      | code, Some next -> loop (code :: instrs) next
+      match codegen_block cfg block visited with
+      | code, Some next -> loop (code :: instrs) next visited
       | code, None ->
         return (code :: instrs), None in
-  loop [] start_block_idx
+  loop [] start_block_idx visited
 
 let codegen (cfg : unit Cfg.t) : unit Instr.t list =
-  fst (codegen_until cfg cfg.entry_block (-1))
+  fst (codegen_until cfg cfg.entry_block (-1) IntSet.empty)
 
 let cfg_to_func_inst (cfg : unit Cfg.t) : Func_inst.t =
   let body: unit Instr.t list = codegen cfg in
