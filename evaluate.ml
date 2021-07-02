d51811487aac42dfe64cca297e57f8aa7bc38907
Quentin Stiévenart <quentin.stievenart@gmail.com>
2021-07-02 13:04:55 +0200

    Update evaluate interface to slice everything

--- evaluate.ml
+++ evaluate.ml
@@ -20,7 +20,7 @@ type result =
   | Ignored of ignored_reason
   | SliceExtensionError of int32 * Instr.Label.t * string
   | SliceError of int32 * Instr.Label.t * string
-  | CfgError of int32 * Instr.Label.t * string
+  | CfgError of int32 * string
   | LoadError of string
 
 let all_labels (instrs : 'a Instr.t list) : Instr.Label.Set.t =
@@ -35,7 +35,7 @@ let time (f : unit -> 'a) : 'a * Time.Span.t =
   let t1 = Time.now () in
   (res, Time.diff t1 t0)
 
-let random_slices (filename : string) : result list =
+let slices (filename : string) (criterion_selection : [`Random | `All | `Last ]) : result list =
   try
     Spec_inference.propagate_globals := false;
     Spec_inference.propagate_locals := false;
@@ -45,35 +45,39 @@ let random_slices (filename : string) : result list =
     if List.is_empty funcs then
       [Ignored NoFunction]
     else
-      List.map funcs ~f:(fun func ->
+      List.concat_map funcs ~f:(fun func ->
           let labels = Instr.Label.Set.to_array (all_labels func.code.body) in
           if Array.length labels = 0 then
-            Ignored (NoInstruction func.idx)
+            [Ignored (NoInstruction func.idx)]
           else
-            let slicing_criterion = Array.get labels (Random.int (Array.length labels)) in
             try
               let (cfg, preanalysis_time) = time (fun () -> Cfg.without_empty_nodes_with_no_predecessors (Spec_analysis.analyze_intra1 wasm_mod func.idx)) in
               let cfg_instructions = Cfg.all_instructions cfg in
-              let t0 = Time.now () in
-              try
-                let instrs_to_keep = Slicing.instructions_to_keep cfg cfg_instructions slicing_criterion in
-                try
-                  let sliced_func = Slicing.slice_alternative_to_funcinst cfg ~instrs:(Some instrs_to_keep) cfg_instructions slicing_criterion in
-                  let t1 = Time.now () in
-                  let slicing_time = Time.diff t1 t0 in
-                  let sliced_labels = all_labels sliced_func.code.body in
-                  Success {
-                    function_sliced = func.idx;
-                    slicing_criterion;
-                    initial_number_of_instrs = Array.length labels;
-                    slice_size_before_adaptation = Instr.Label.Set.length instrs_to_keep;
-                    slice_size_after_adaptation = Instr.Label.Set.length sliced_labels;
-                    preanalysis_time;
-                    slicing_time;
-                  }
-                with e -> SliceExtensionError (func.idx, slicing_criterion, Exn.to_string e)
-              with e -> SliceError (func.idx, slicing_criterion, Exn.to_string e)
-            with e -> CfgError (func.idx, slicing_criterion, Exn.to_string e))
+              List.map (match criterion_selection with
+                  | `Random -> [Array.get labels (Random.int (Array.length labels))]
+                  | `All -> Array.to_list labels
+                  | `Last -> [Array.last labels])
+                ~f:(fun slicing_criterion ->
+                    let t0 = Time.now () in
+                    try
+                      let instrs_to_keep = Slicing.instructions_to_keep cfg cfg_instructions slicing_criterion in
+                      try
+                        let sliced_func = Slicing.slice_alternative_to_funcinst cfg ~instrs:(Some instrs_to_keep) cfg_instructions slicing_criterion in
+                        let t1 = Time.now () in
+                        let slicing_time = Time.diff t1 t0 in
+                        let sliced_labels = all_labels sliced_func.code.body in
+                        Success {
+                          function_sliced = func.idx;
+                          slicing_criterion;
+                          initial_number_of_instrs = Array.length labels;
+                          slice_size_before_adaptation = Instr.Label.Set.length instrs_to_keep;
+                          slice_size_after_adaptation = Instr.Label.Set.length sliced_labels;
+                          preanalysis_time;
+                          slicing_time;
+                        }
+                      with e -> SliceExtensionError (func.idx, slicing_criterion, Exn.to_string e)
+                    with e -> SliceError (func.idx, slicing_criterion, Exn.to_string e))
+            with e -> [CfgError (func.idx, Exn.to_string e)])
   with e -> [LoadError (Exn.to_string e)]
 
 
@@ -83,11 +87,11 @@ let output (file : string) (fields : string list) =
   Out_channel.with_file (Printf.sprintf "%s/%s" !prefix file) ~append:true ~f:(fun ch ->
       Out_channel.output_string ch (Printf.sprintf "%s\n" (String.concat ~sep:"," fields)))
 
-let evaluate_files (files : string list) =
+let evaluate_files (files : string list) (criterion_selection : [`All | `Random | `Last]) : unit =
   let total = List.length files in
   List.iteri files ~f:(fun i filename ->
       Printf.printf "\r%d/%d %s%!" i total filename;
-      List.iter (random_slices filename) ~f:(function
+      List.iter (slices filename criterion_selection) ~f:(function
       | Success r ->
         output "data.txt" [filename; Int32.to_string r.function_sliced; Instr.Label.to_string r.slicing_criterion;
                            string_of_int r.initial_number_of_instrs;
@@ -105,8 +109,8 @@ let evaluate_files (files : string list) =
       | SliceError (f, criterion, reason) ->
         output "error.txt" [filename; Int32.to_string f; Instr.Label.to_string criterion;
                             "slice"; reason]
-      | CfgError (f, criterion, reason) ->
-        output "error.txt" [filename; Int32.to_string f; Instr.Label.to_string criterion;
+      | CfgError (f, reason) ->
+        output "error.txt" [filename; Int32.to_string f; "-1";
                             "cfg"; reason]
       | LoadError _ ->
         output "loaderror.txt" [filename]))
@@ -116,11 +120,13 @@ let evaluate =
     ~summary:"Evalate the slicer on a set of benchmarks, listed in the file given as argument"
     Command.Let_syntax.(
       let%map_open filelist = anon ("filelist" %: string)
-      and prefix_opt = anon (maybe ("prefix" %: string)) in
+      and prefix_opt = flag "-p" (optional string) ~doc:"prefix for where to save the results file (default: current directory)"
+      and all = flag "-a" no_arg ~doc:"slice on all functions, for all slicing criterion"
+      and last = flag "-l" no_arg ~doc:"slice on all functions, for the last slicing criterion" in
       fun () ->
         match prefix_opt with
         | None -> ()
         | Some p -> prefix := p
           ;
-        evaluate_files (In_channel.read_lines filelist))
+        evaluate_files (In_channel.read_lines filelist) (if all then `All else if last then `Last else `Random))
 
